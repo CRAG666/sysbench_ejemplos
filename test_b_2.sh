f5c4e98ce0f3681eb0825358bc79d54efd195652
Crag <ogeidca49@gmail.com>
2023-05-09 00:28:22 -0600

    change

--- test_b_2.sh
+++ test_b_2.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-sysbench --db-driver=mysql --mysql-host=localhost --mysql-user=root --mysql-password=mypassword --mysql-db=mydatabase --table-size=20000000 --tables=1 --threads=16 --time=60 --report-interval=10 /usr/share/sysbench/oltp_write_only.lua prepare
+sysbench --db-driver=mysql --mysql-host=<HOST> --mysql-port=<PORT> --mysql-user=<USER> --mysql-password=<PASSWORD> --mysql-db=<DB> --table-size=<TABLE SIZE> --tables=<NUM OF TABLES> --threads=<NUM OF THREADS> --time=<DURATION> --report-interval=<REPORT INTERVAL> oltp_write_only.lua prepare
+sysbench --db-driver=mysql --mysql-host=<HOST> --mysql-port=<PORT> --mysql-user=<USER> --mysql-password=<PASSWORD> --mysql-db=<DB> --table-size=<TABLE SIZE> --tables=<NUM OF TABLES> --threads=<NUM OF THREADS> --time=<DURATION> --report-interval=<REPORT INTERVAL> oltp_write_only.lua run
+sysbench --db-driver=mysql --mysql-host=<HOST> --mysql-port=<PORT> --mysql-user=<USER> --mysql-password=<PASSWORD> --mysql-db=<DB> --table-size=<TABLE SIZE> --tables=<NUM OF TABLES> --threads=<NUM OF THREADS> --time=<DURATION> --report-interval=<REPORT INTERVAL> oltp_write_only.lua cleanup
 
-sysbench --db-driver=mysql --mysql-host=localhost --mysql-user=root --mysql-password=mypassword --mysql-db=mydatabase --table-size=20000000 --tables=1 --threads=16 --time=60 --report-interval=10 /usr/share/sysbench/oltp_write_only.lua run
-
-sysbench --db-driver=mysql --mysql-host=localhost --mysql-user=root --mysql-password=mypassword --mysql-db=mydatabase --table-size=20000000 --tables=1 --threads=16 --time=60 --report-interval=10 /usr/share/sysbench/oltp_write_only.lua cleanup
